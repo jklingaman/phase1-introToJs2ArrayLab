2ef969c9912ff3d38a7c8c62b5e796f0175f766a
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-02-18 12:39:52 -0600

    passes all test

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+    return cats;
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats;
+}
+
+function appendCat() {
+    const newCats = [...cats];
+    newCats.push('Broom');
+    return newCats;
+}
+
+function prependCat() {
+    const newCats = [...cats];
+    newCats.unshift('Arnold')
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = [...cats]
+    newCats.pop();
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = [...cats]
+    newCats.shift();
+    return newCats;
+}
